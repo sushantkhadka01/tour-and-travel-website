b8888230081d2c563edc6478bdda3010c98acf8e
Sushant Khadka <sushantkhadka1111@gmail.com>
2024-06-09 12:23:01 +0545

    initial commit of sign in form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SigninForm from './SigninForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SigninForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/images.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import rma from './images/rma.png';
+function images() {
+  return (
+    <div> images </div>
+    <img src={rma} />
+  )
+}
+
+export default images
