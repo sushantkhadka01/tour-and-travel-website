7ebf380ed28bd47c273db95493e65419960a8800
Sushant Khadka <sushantkhadka1111@gmail.com>
2024-07-25 11:04:50 +0545

    Added Bootstrap

--- src/Services.js
+++ src/Services.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Header from './Header'; // Import Header component
+import { Container, Row, Col } from 'react-bootstrap';
+import Header from './Header'; 
 import './Services.css';
 import madridImage from './assets/madrid.jpg'; 
 import adventureImage from './assets/adventure.jpg'; 
@@ -37,22 +38,29 @@ const services = [
 
 const Services = () => {
   return (
-    <div className="services">
+    <div className="home-page">
       <Header /> 
-      <div className="header-container">
-        <h2>Our Services</h2>
-      </div>
-      <div className="services-list">
-        {services.map((service, index) => (
-          <div className="service-card" key={index}>
-            <img src={service.image} alt={service.title} />
-            <h3>{service.title}</h3>
-            <p>{service.description}</p>
-          </div>
-        ))}
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <h1>Our Services</h1>
+            <p>Welcome to our Siuuuushant travel and tours company, where your dream adventures come to life! We specialize in curating unforgettable experiences, whether you're looking to explore vibrant cityscapes, embark on thrilling adventures, unwind on serene beach holidays, immerse yourself in rich cultural experiences, or embark on exciting wildlife safaris.</p>
+            <div className="services-list">
+              {services.map((service, index) => (
+                <div className="service-card" key={index}>
+                  <img src={service.image} alt={service.title} />
+                  <h3>{service.title}</h3>
+                  <p>{service.description}</p>
+                </div>
+              ))}
+            </div>
+            <p>Join us and let us turn your travel dreams into reality. Our dedicated team is committed to providing exceptional service and ensuring every journey is a memorable one.</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
 
 export default Services;
+
